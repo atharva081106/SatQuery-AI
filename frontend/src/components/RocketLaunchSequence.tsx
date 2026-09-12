e92c707a8d9e283083073a41e12e5e07eb204935
atharva081106 <atharva081106@gmail.com>
2026-09-12 19:26:07 +0530

    feat(gallery): add moon to the background of the rocket launch sequence

--- frontend/src/components/RocketLaunchSequence.tsx
+++ frontend/src/components/RocketLaunchSequence.tsx
@@ -43,6 +43,7 @@ function LaunchAnimation() {
 
   const earthTexture = useTexture('/textures/2k_earth_daymap.jpg');
   const cloudsTexture = useTexture('/textures/2k_earth_clouds.jpg');
+  const moonTexture = useTexture('/textures/2k_mercury.jpg'); // Looks identical to moon!
 
   useFrame((state, delta) => {
     const offset = scroll.offset;
@@ -279,6 +280,14 @@ function LaunchAnimation() {
         <mesh>
           <ringGeometry args={[21.95, 22.05, 128]} />
           <meshBasicMaterial color="#00ffcc" transparent opacity={0.15} side={THREE.DoubleSide} />
+        </mesh>
+      </group>
+
+      {/* MOON SYSTEM */}
+      <group position={[-60, 40, -150]}>
+        <mesh>
+          <sphereGeometry args={[12, 64, 64]} />
+          <meshStandardMaterial map={moonTexture} roughness={0.9} metalness={0.1} color="#cccccc" />
         </mesh>
       </group>
 
